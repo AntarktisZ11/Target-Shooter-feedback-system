249e488af15c5a7a475079f63e8abbc20703a125
Marcus Börne <marcus.borne@outlook.com>
2021-05-21 02:59:46 +0200

    socket testing 2

---
+++ test_socket_client.py
@@ -0,0 +1,16 @@
+import socket
+
+HOST = '192.168.1.4'
+PORT = 12345
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("Pre connection")
+s.connect((HOST, PORT))
+print("Connected")
+
+while True:
+    command = input("Enter your command: ")
+    s.send(command.encode())
+    reply = s.recv(1024).decode()
+    print(reply)
+    if command == 'break':
+        break
