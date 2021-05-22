77d47aa2adaa37d2c205554c4a2957be5c230a0f
Marcus Börne <marcus.borne@outlook.com>
2021-05-22 04:52:21 +0200

    Passing DataFrame as csv only storing in buffer

--- test_socket_client.py
+++ test_socket_client.py
@@ -1,5 +1,7 @@
 import socket
+import pandas as pd
 import pickle
+from io import StringIO
 
 HOST = '192.168.1.4'
 PORT = 12345
@@ -13,7 +15,14 @@ while True:
     s.send(command.encode())
 
     if command == 'df':
-        print(pickle.loads(s.recv(4096)))
+        df = pickle.loads(s.recv(4096))
+        print(df)
+    elif command == 'df_csv':
+        buffer = StringIO()
+        s.recv_into(buffer)
+        print(buffer.getvalue())
+        df = pd.read_csv(buffer)
+        print(df)
     else:
         reply = s.recv(1024).decode()
         print(reply)
--- test_socket_server.py
+++ test_socket_server.py
@@ -1,4 +1,8 @@
 import socket
+from numpy import diag_indices_from
+import pandas as pd
+import pickle
+from io import StringIO
 
 HOST = '192.168.1.4'
 PORT = 12345
@@ -11,19 +15,32 @@ except socket.error:
     print('Bind failed')
 
 
+df = pd.DataFrame([[0,1,2], [1,2,3], [2,3,4]])
+print(df)
+df_bytes = pickle.dumps(df)
+print(df_bytes.__sizeof__())
+
+
 s.listen(1)
 print("Socket awaitning messages")
 (conn, addr) = s.accept()
 print("Connected")
+
 
 while True:
     data = conn.recv(1024).decode()
     print("I sent a message back in response to: " + data)
 
     if data == 'break':
-        conn.send("Breaking connection".encode())
+        conn.sendall("Breaking connection".encode())
         break
+    elif data == 'df':
+        conn.sendall(df_bytes)
+    elif data == 'df_csv':
+        buffer = StringIO()
+        df.to_csv(buffer)
+        conn.sendfile(buffer.getvalue())
     else:
         reply = "Hello, world!"
-        conn.send(reply.encode())
+        conn.sendall(reply.encode())
 conn.close()
