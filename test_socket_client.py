b0b5c3b1e33fdbcf8ef9cc0519bd2f065ecf49a2
Marcus Börne <marcus.borne@outlook.com>
2021-05-25 03:14:57 +0200

    Functionall socket test client, has reconnection logic and DataFrame reconstruction

--- test_socket_client.py
+++ test_socket_client.py
@@ -1,31 +1,50 @@
 import socket
 import pandas as pd
 import pickle
-from io import StringIO
 
 HOST = '192.168.1.4'
 PORT = 12345
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 print("Pre connection")
-s.connect((HOST, PORT))
-print("Connected")
+def connect():
+    global s
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    while True:
+        try:
+            s.connect((HOST, PORT))
+            break
+        except TimeoutError:
+            print("Waiting to connect!")
+    print("Connected")
 
-while True:
-    command = input("Enter your command: ")
-    s.send(command.encode())
+def main():
+    connect()
+    while True:
+        command = input("Enter your command: ")
+        try:
+            s.send(command.encode())
+        except (ConnectionResetError, ConnectionAbortedError) as e:
+            print(e)
+            print("Error: Lost connection - Reconnecting")
+            command = None
+            main()
 
-    if command == 'df':
-        df = pickle.loads(s.recv(4096))
-        print(df)
-    elif command == 'df_csv':
-        buffer = StringIO()
-        s.recv_into(buffer)
-        print(buffer.getvalue())
-        df = pd.read_csv(pickle.loads(buffer))
-        print(df)
-    else:
-        reply = s.recv(1024).decode()
-        print(reply)
+        if command == 'df':
+            df = pickle.loads(s.recv(4096))
+            print(df)
+        elif command == 'df_csv':
+            pickled_data = s.recv(1024)
+            unpickled_data = pickle.loads(pickled_data)
+            unpickled_data.seek(0)
+            df = pd.read_csv(unpickled_data,index_col=0)
+            print(df)
+        else:
+            reply = s.recv(1024).decode()
+            print(reply)
 
-    if command == 'break':
-        break
+        if command == 'break':
+            s.close()
+            return
+
+if __name__ == '__main__':
+    main()
