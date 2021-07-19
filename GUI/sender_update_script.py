83e6a9759b5f5b3b7463e0ca62933f1c151d3514
Marcus Börne <marcus.borne@outlook.com>
2021-07-19 23:50:00 +0200

    Changed local folder if linux

--- GUI/sender_update_script.py
+++ GUI/sender_update_script.py
@@ -1,3 +1,5 @@
+import sys
+import os
 import urllib.request  # the lib that handles the url stuff
 
 #* Is self-updating
@@ -12,7 +14,12 @@ github_user = "AntarktisZ11"
 repo_parent = "Target-Shooter-feedback-system/master/GUI"
 base_url = f'{site}/{github_user}/{repo_parent}/'
 
-local_project_folder = r"C:/Users/Marcus Börne/Desktop/Python Projects/L-O/Target-Shooter-feedback-system/GUI/temp/"
+if sys.platform == "linux":
+    prog_call = sys.argv[0]
+    prog_location = os.path.split(prog_call)[0]
+    local_project_folder = prog_location
+else:
+    local_project_folder = r"C:/Users/Marcus Börne/Desktop/Python Projects/L-O/Target-Shooter-feedback-system/GUI/temp/"
 
 for filename in filenames:
     url = base_url + filename
